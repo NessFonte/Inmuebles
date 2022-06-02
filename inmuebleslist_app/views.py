a92a8b5423116a64a924182e3bc15cca072caa9b
NessFonte <neisser2909@gmail.com>
2022-06-02 12:02:36 -0400

    Función busqueda por id creada

--- inmuebleslist_app/views.py
+++ inmuebleslist_app/views.py
@@ -1,4 +1,3 @@
-from optparse import Values
 from django.shortcuts import render
 from inmuebleslist_app.models import Inmueble
 from django.http import JsonResponse
@@ -9,6 +8,18 @@ def inmueble_list(request):
     inmuebles = Inmueble.objects.all()
     data = {
         'inmuebles': list(inmuebles.values())
+    }
+    
+    return JsonResponse(data)
+
+def inmueble_detalle(request, pk):
+    inmueble = Inmueble.objects.get(pk=pk)
+    data = {
+        'direccion': inmueble.direccion,
+        'pais': inmueble.pais,
+        'descripcion': inmueble.descripcion,
+        'imagen': inmueble.imagen,
+        'active': inmueble.active,
     }
     
     return JsonResponse(data)
