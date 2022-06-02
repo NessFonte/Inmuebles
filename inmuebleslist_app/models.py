74ac5ca3f97fa4b754bd0fcd4b4712337a7add3f
NessFonte <neisser2909@gmail.com>
2022-06-02 10:17:03 -0400

    Clase Inmueble creada en modles.py

--- inmuebleslist_app/models.py
+++ inmuebleslist_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Inmueble(models.Model):
+    direccion = models.CharField(max_length=250)
+    pais = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=500)
+    imagen = models.CharField(max_length=900)
+    active = models.BooleanField(default=True)
